24a168932e44a5fec58af75e804d04fb11214081
Christopher Jacobs <chrisj.3222@gmail.com>
2019-04-14 19:10:43 +0000

    Automatically backed up by Learn

--- lib/players/human.rb
+++ lib/players/human.rb
@@ -1,14 +1,8 @@
 module Players
-  include Player
+end
+  
   class Human < Player
     def move(board)
       # Code for #board goes here
     end
   end
-end
-
-class Player
-  def initialize(token)
-    @token = token
-  end
-end
